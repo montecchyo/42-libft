495cd8555ea775a542bd3b3212d3e57cc1b0529a
Klevlon Moraes <montecchyo@gmail.com>
2021-10-12 00:40:26 +0200

    Correção strjoin 5

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: kborges- <kborges-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/03 10:23:17 by kborges-          #+#    #+#             */
-/*   Updated: 2021/10/12 00:36:48 by kborges-         ###   ########.fr       */
+/*   Updated: 2021/10/12 00:40:19 by kborges-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,12 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		return (NULL);
 	i = 0;
 	j = 0;
-	while (i < ft_strlen(s1))
+	while (s1[i])
 	{
 		strj[i] = s1[i];
 		i++;
 	}
-	while (j < total_len(s2))
+	while (s2[j])
 	{
 		strj[i + j] = s2[j];
 		j++;
